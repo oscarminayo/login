dd90c60922d5a989eb1cbfd350ec7101faf21e59
Curso <Curso@Formacion-TOS10.ipartekaula.com>
2019-01-18 12:51:37 +0100

    Filtro añadido al login.

---
+++ src/main/java/filters/SeguridadFilter.java
@@ -0,0 +1,72 @@
+package filters;
+
+import java.io.IOException;
+import javax.servlet.DispatcherType;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import pojos.Usuario;
+
+/**
+ * Servlet Filter implementation class SeguridadFilter
+ */
+@WebFilter(dispatcherTypes = {
+				DispatcherType.REQUEST, 
+				DispatcherType.FORWARD, 
+				DispatcherType.INCLUDE, 
+				DispatcherType.ERROR
+		}
+					, urlPatterns = { "/privado/*" })
+public class SeguridadFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public SeguridadFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest)request;
+		HttpServletResponse res = (HttpServletResponse)response;
+		
+		HttpSession session = req.getSession();
+		Usuario uLogeado = (Usuario)session.getAttribute("usuario");
+		
+		if(uLogeado != null) {
+			chain.doFilter(request, response);
+			
+		}else {
+			// redigir al login, porque el usuario se ha saltado el login o caducada session
+			res.sendRedirect("/login");
+		}	
+		
+		
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
